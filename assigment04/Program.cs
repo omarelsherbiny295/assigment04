bc5169300930472ef394e3c65dbb5d0b2aef5fb9
Elsherbinyomarayman2004 <Elsherbinyomarayman2004@gmail.com>
2026-09-06 08:14:58 +0300

    question1

--- assigment04/Program.cs
+++ assigment04/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region question1
+            double[] prices = { 25.5, 40.0, 33.75 };
+            Console.WriteLine(prices[1]);
+
+            #endregion
         }
     }
 }
